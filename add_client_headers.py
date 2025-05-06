bc506a33e19104afc8872967c983554363db8f82
GGasset <gassetgerman@gmail.com>
2025-05-06 22:29:16 +0200

    Adjusted CMakeLists for folder structure improvements

--- add_client_headers.py
+++ add_client_headers.py
@@ -1,9 +1,10 @@
 def main():
     copy_directory = ['socket_client']
+    source_directory = 'Separate_headers'
     copy_filenames = ['NN_enums.h', 'data_type.h']
     for file_name in copy_filenames:
         content = ''
-        with open(file_name, 'r') as file:
+        with open(f"{source_directory}/{file_name}", 'r') as file:
             content = file.read()
         for directory in copy_directory:
             with open(f"{directory}/{file_name}", 'w') as file:
