4083fecb2c6fdbd9878017050a775ec2c80c77f6
GGasset <gassetgerman@gmail.com>
2025-05-11 20:32:59 +0200

    finished without testing optimizer_init functions

--- Layers/Optimizers/Optimizer_init.cu
+++ Layers/Optimizers/Optimizer_init.cu
@@ -1,7 +1,7 @@
 
 #include "Optimizer_init.h"
 
-__device__ IOptimizer* initialize_optimizer(optimizers_enum optimizer)
+__device__ IOptimizer* initialize_optimizer(optimizers_enum optimizer, size_t parameter_count)
 {
 	IOptimizer* out = 0;
 
@@ -9,8 +9,13 @@ __device__ IOptimizer* initialize_optimizer(optimizers_enum optimizer)
 	{
 	case no_optimizer:
 		cudaMalloc(&out, sizeof(IOptimizer));
+		*out = IOptimizer();
+		out->alloc_optimizer_values(parameter_count, false);
 		break;
 	case Adam:
+		cudaMalloc(&out, sizeof(AdamOptimizer));
+		*out = AdamOptimizer();
+		out->alloc_optimizer_values(parameter_count, false);
 		break;
 	default:
 		return (0);
@@ -18,12 +23,12 @@ __device__ IOptimizer* initialize_optimizer(optimizers_enum optimizer)
 	return (out);
 }
 
-__global__ void global_optimizer_init(optimizers_enum optimizer, IOptimizer** out)
+__global__ void global_optimizer_init(optimizers_enum optimizer, IOptimizer** out, size_t parameter_count)
 {
 	*out = initialize_optimizer(optimizer);
 }
 
-__host__ IOptimizer* host_optimizer_init(optimizers_enum optimizer)
+__host__ IOptimizer* host_optimizer_init(optimizers_enum optimizer, size_t parameter_count)
 {
 	IOptimizer** tmp = 0;
 	cudaMalloc(&tmp, sizeof(IOptimizer*));
@@ -32,5 +37,6 @@ __host__ IOptimizer* host_optimizer_init(optimizers_enum optimizer)
 
 	IOptimizer* out = 0;
 	cudaMemcpy(&out, tmp, sizeof(IOptimizer*), cudaMemcpyDeviceToHost);
+	cudaFree(tmp);
 	return (out);
 }
--- Layers/Optimizers/Optimizer_init.h
+++ Layers/Optimizers/Optimizer_init.h
@@ -4,6 +4,6 @@
 #include "Adam.h"
 
 #pragma once
-__device__ IOptimizer* initialize_optimizer(optimizers_enum optimizer);
-__global__ void global_optimizer_init(optimizers_enum optimizer, IOptimizer **out);
-__host__ IOptimizer* host_optimizer_init(optimizers_enum optimizer);
+__device__ IOptimizer* initialize_optimizer(optimizers_enum optimizer, size_t parameter_count);
+__global__ void global_optimizer_init(optimizers_enum optimizer, IOptimizer **out, size_t parameter_count);
+__host__ IOptimizer* host_optimizer_init(optimizers_enum optimizer, size_t parameter_count);
