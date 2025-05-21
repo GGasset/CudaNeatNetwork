746da9f217fd84e5c5e261b9168b55f75b0c6377
GGasset <gassetgerman@gmail.com>
2025-05-21 13:50:41 +0200

    added function decalarations for saving optimizers to disk

--- Layers/Optimizers/Optimizer_init.h
+++ Layers/Optimizers/Optimizer_init.h
@@ -8,3 +8,9 @@ __device__ IOptimizer* initialize_optimizer(optimizers_enum optimizer, size_t pa
 __global__ void global_optimizer_init(optimizers_enum optimizer, IOptimizer **out, size_t parameter_count);
 __host__ IOptimizer* host_optimizer_init(optimizers_enum optimizer, size_t parameter_count);
 __global__ void call_Optimizer_destructor(IOptimizer *optimizer);
+
+
+// SAVING - LOADING
+
+__global__ void get_optimizer_data_buffer(IOptimizer *optimizer, void **out_buffer);
+__host__ void host_save_optimizer(FILE *file, IOptimizer *optimizer);
