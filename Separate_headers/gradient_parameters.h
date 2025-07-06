6885b5856b3c7fe606b1335b20d12c92e47f2600
GGasset <gassetgerman@gmail.com>
2025-07-06 21:51:11 +0200

    modified default gradient clip value

--- Separate_headers/gradient_parameters.h
+++ Separate_headers/gradient_parameters.h
@@ -6,7 +6,7 @@
 typedef struct gradient_hyperparameters
 {
 	data_t learning_rate = .01;
-	data_t gradient_clip = 50;
+	data_t gradient_clip = 1;
 	float  dropout_rate = .2;
 } gradient_hyperparameters;
 
