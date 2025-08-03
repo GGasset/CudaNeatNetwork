00fc8f7cd5735d7100d7c5937887f4240ea007ac
GGasset <gassetgerman@gmail.com>
2025-08-03 14:53:59 +0200

    added regularization and entropy bonus hyperparameters

--- Separate_headers/gradient_parameters.h
+++ Separate_headers/gradient_parameters.h
@@ -3,11 +3,29 @@
 
 #include "data_type.h"
 
+// Entropy regulariztion, adds a bonus term to the loss that moves values towards + - .367
+//	-.367 is added for compatibility with negative activation functions such as tanh
+// Encourages exploration and prevents suboptimal convergence
+typedef struct
+{
+	bool active = false;
+
+	// Sets the strength of the regularization, normally 0.01 or less
+	data_t entropy_coefficient = .01;
+} entropy_bonus_hyperparameters;
+
+typedef struct
+{
+	entropy_bonus_hyperparameters entropy_bonus;
+} regularization_hyperparameters;
+
 typedef struct gradient_hyperparameters
 {
 	data_t learning_rate = .01;
 	data_t gradient_clip = 1;
 	float  dropout_rate = .2;
+
+	regularization_hyperparameters regularization;
 } gradient_hyperparameters;
 
 typedef struct GAE_hyperparameters
