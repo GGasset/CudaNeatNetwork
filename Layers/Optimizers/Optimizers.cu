8b46c4f7a44d5717e9c181ea4263c225fe164b56
GGasset <gassetgerman@gmail.com>
2025-09-18 19:19:40 +0200

    started implementing Optimizers class

---
+++ Layers/Optimizers/Optimizers.cu
@@ -0,0 +1,90 @@
+
+#include "Optimizers.h"
+
+void Optimizers::copy_values_of(size_t optimizer_i, Optimizers dst)
+{
+    Optimizer_values dst_values = dst.optimizer_values[optimizer_i];
+    Optimizer_values src_values = optimizer_values[optimizer_i];
+
+    if (dst_values.value_count_per_parameter != src_values.value_count_per_parameter) throw;
+    if (dst.parameter_count != parameter_count) throw;
+    if (!dst_values.values || !src_values.values) return ;
+
+    size_t arr_len = src_values.value_count_per_parameter * parameter_count;
+    cudaMemcpy(dst_values.values, src_values.values, sizeof(data_t) * arr_len, cudaMemcpyDeviceToDevice);
+}
+
+void Optimizers::save_values_of(size_t optimizer_i, FILE *file)
+{
+    Optimizer_values optimizer = optimizer_values[optimizer_i];
+    size_t len = parameter_count * optimizer.value_count_per_parameter;
+    if (!len) return;
+    save_array(optimizer.values, len, file, true);
+}
+
+Optimizer_values Optimizers::load_values_of(size_t optimizer_i, FILE *file)
+{
+    Optimizer_values out;
+
+    size_t values_per_param = optimizer_values_per_parameter[optimizer_i];
+    out.value_count_per_parameter = values_per_param;
+    size_t arr_len = values_per_param * parameter_count;
+    if (!arr_len) return out;
+
+    out.values = load_array<data_t>(arr_len, file, true);
+}
+
+void Optimizers::allocate_values()
+{
+    for (size_t i = 0; i < optimizers_enum::last_optimizer_entry; i++)
+    {
+        if (!optimizer_values[i].value_count_per_parameter)
+            continue;
+        cudaFree(optimizer_values[i].values);
+        cudaMalloc(&optimizer_values[i].values, sizeof(data_t) * parameter_count * optimizer_values[i].value_count_per_parameter);
+    }
+}
+
+Optimizers::Optimizers()
+{
+    optimizer_values_per_parameter[Adam] = 7;
+    optimizer_values_per_parameter[ElasticNet] = 2;
+
+    for (size_t i = 0; i < last_optimizer_entry; i++)
+        optimizer_values[i].value_count_per_parameter 
+            = optimizer_values_per_parameter[i];
+}
+
+Optimizers::Optimizers(size_t parameter_count, optimizers_hyperparameters optimizer_options)
+{
+    *this = Optimizers();
+
+    this->parameter_count = parameter_count;
+    initialization_hyperparameters = optimizer_options;
+
+    allocate_values();
+    initialize_values(0, parameter_count);
+}
+
+Optimizers Optimizers::Clone()
+{
+    Optimizers out = Optimizers(parameter_count, initialization_hyperparameters);
+    for (size_t i = 0; i < last_optimizer_entry; i++)
+        copy_values_of(i, out);
+    return out;
+}
+
+Optimizers::~Optimizers()
+{
+    for (size_t i = 0; i < optimizers_enum::last_optimizer_entry; i++)
+    {
+        cudaFree(optimizer_values[i].values);
+        optimizer_values[i].values = 0;
+    }
+    
+}
+
+void Optimizers::set_initialization(optimizers_hyperparameters new_initialization)
+{
+    initialization_hyperparameters = new_initialization;
+}
--- Layers/Optimizers/Optimizers.h
+++ Layers/Optimizers/Optimizers.h
@@ -2,26 +2,64 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdio>
 
 #include "data_type.h"
 #include "NN_enums.h"
 #include "gradient_parameters.h"
 
+#include "cuda_functionality.cuh"
 
-typedef struct Optimizer_values
+typedef struct
 {
-	data_t  *values;
-	size_t  value_count_per_parameter;
-};
+	data_t  *values = 0;
+	size_t  value_count_per_parameter = 0;
+} Optimizer_values;
 
 class Optimizers
-{
+{	
 private:
-	Optimizer_values optimizer_values[optizers_enum::last_optimizer_entry];
-	size_t parameter_count;
+	/*
+	Steps to add an optimizer:
+
+	Add it to optimizers enum before last optimizer_entry and without a set value
+	Add its value_count per parameter to Optimizers default constructor
+	*/
+	Optimizer_values optimizer_values[last_optimizer_entry];
+	size_t parameter_count = 0;
+	int optimizer_values_per_parameter[last_optimizer_entry];
+	optimizers_hyperparameters initialization_hyperparameters;
+
+	// Does not allocate values
+	void copy_values_of(size_t optimizer_i, Optimizers dst);
 	
-public:	
+	void save_values_of(size_t optimizer_i, FILE *file);
+	Optimizer_values load_values_of(size_t optimizer_i, FILE *file);
+
+	void initialize_values(long parameter_start_i, size_t parameter_count);
+	void allocate_values();
+
+public:
 	Optimizers();
-	Optimizers(optimizers_hyperparameters optimizer_options);
+	// optimizer_options is used for hyperparameter initialization
+	Optimizers(size_t parameter_count, optimizers_hyperparameters optimizer_options);
+	Optimizers Clone();
 	~Optimizers();
+
+	void save(FILE *file);
+	static Optimizers *load(FILE *file);
+
+	// sets the hyperparameters that will be used for initialization
+	void set_initialization(optimizers_hyperparameters new_initialization);
+
+	// Adds values to fit new parameters, also initializes them
+	//
+	// block_start_i: the start position of the inserted parameters, < 0 to append (i.e. -1)
+	void add_parameters(size_t added_parameter_count, long insert_i);
+
+	// Removes values to adjust for deleted parameters
+	//
+	// block_start_i: the start position of the deleted parameters, < 0 to remove from the end (i.e. -1)
+	void remove_parameters(size_t removed_count, long removed_i);
+
 };
