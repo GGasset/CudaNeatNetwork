c3fde1d066a02ffb8407f44fafd7463cd11e53e3
GGasset <gassetgerman@gmail.com>
2025-10-10 17:57:56 +0200

    fixed bug

--- Layers/LSTMLayer.cu
+++ Layers/LSTMLayer.cu
@@ -32,18 +32,13 @@ void LSTMLayer::layer_specific_initialize_fields(size_t connection_count, size_t
 {
 	size_t neuron_weights_count = sizeof(data_t) * neuron_count * 4;
 	cudaMalloc(&state, sizeof(data_t) * neuron_count * 2);
-	cudaMalloc(&neuron_weights, sizeof(field_t) * neuron_count * 4);
+	cudaMalloc(&neuron_weights, sizeof(field_t) * neuron_weights_count);
 	cudaMalloc(&prev_state_derivatives, sizeof(data_t) * neuron_count * 3);
 
 	cudaMemset(state, 0, sizeof(data_t) * neuron_count * 2);
 	cudaMemset(prev_state_derivatives, 0, sizeof(data_t) * neuron_count * 3);
-	cudaMemset(neuron_weights, 0, sizeof(field_t) * neuron_count * 4);
-	generate_random_values(neuron_weights, neuron_count * 4, 0, Xavier_uniform_initialization_scale_factor(neuron_count, neuron_count), true);
-	
-	add_to_array kernel(neuron_weights_count / 32 + (neuron_weights_count % 32 > 0), 32) (
-		neuron_weights, neuron_weights_count, 1
-	);
-	cudaDeviceSynchronize();
+	cudaMemset(neuron_weights, 0, sizeof(field_t) * neuron_weights_count);
+	generate_random_values(neuron_weights, neuron_weights_count, 0, 1 / Xavier_uniform_initialization_scale_factor(neuron_count, neuron_count), true);
 }
 
 ILayer* LSTMLayer::layer_specific_clone()
