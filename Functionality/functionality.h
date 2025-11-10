3a0e1f7b8e57843f233635d7d943704a305a427b
GGMan <76014247+GGasset@users.noreply.github.com>
2025-11-10 18:01:51 +0100

    Add vec_shuffle_inplace template function
    Added a template function for shuffling a vector in place.

--- Functionality/functionality.h
+++ Functionality/functionality.h
@@ -16,6 +16,13 @@ t h_min(t a, t b)
 	return a * (a <= b) + b * (a > b);
 }
 
+template<typename t>
+void vec_shuffle_inplace(std::vector<t> &inp)
+{
+	size_t rand_val = rand() % (inp.size() - 1);
+	for (size_t i = 0; i < inp.size(); i++, rand_val = rand() % (inp.size() - 1)) inp[i] = inp[rand_val + (rand_val >= i)];
+}
+
 unsigned long long get_arbitrary_number();
 
 float get_random_float();
