aee1a5b176041ee256950097aa958cfb2fe5845d
GGasset <gassetgerman@gmail.com>
2025-11-28 07:02:11 +0100

    added test_env header

---
+++ tests/test_env.h
@@ -0,0 +1,26 @@
+
+#pragma once
+#include <cstddef>
+#include <vector>
+#include "data_type.h"
+
+// test_env is a simple RL grid pathfinding prototype to test PPO
+class test_env
+{
+private:
+	static const size_t board_size = 3;
+
+	std::vector<std::tuple<size_t, size_t>> target_agent_pos;
+	size_t nenvs;
+
+public:
+	test_env(size_t _n_envs);
+
+	std::vector<data_t> get_observations(size_t env_i);
+
+	// Returns reward and wheter the episode finished
+	// Actions_probs must be a host arr
+	std::tuple<data_t, bool> step(data_t *actions_probs, size_t env_i);
+
+
+};
