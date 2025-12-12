d582a82638067ebc885bf55566156faac9c53b10
GGMan <76014247+GGasset@users.noreply.github.com>
2025-12-12 19:27:33 +0100

    Refactor socket_parser read_arr and expected_read_arr
    Refactor read_arr to return a tuple with data and length. Update expected_read_arr to handle errors correctly.

--- Functionality/socket_utils.cpp
+++ Functionality/socket_utils.cpp
@@ -8,21 +8,22 @@ socket_parser::~socket_parser()
 
 void* socket_parser::expected_read_arr(long expected_len)
 {
+	auto [out, len] = read_arr();
+	if (expected_len >= 0 && len != expected_len) {*params.err = true; return 0;}
+	return out;
+}
+
+std::tuple<void *, size_t> socket_parser::read_arr()
+{
 	CHECK_ERRS();
 
 	size_t len = read_var<size_t>(params);
-	if (expected_len >= 0 && len != expected_len) HANDLE_ERR();
-	if (*params.buff_pos + len > params.buff_len) HANDLE_ERR();
+	if (*params.buff_pos + len > params.buff_len) {*params.err = true; return {0,0};}
 
 	char *out = new char[len + 1];
 	if (!out) HANDLE_ERR();
 	out[len] = 0;
 	for (size_t i = 0; i < len; i++) out[i] = read_var<char>();
 	gathered_pointers.push_back(out);
-	return out;
-}
-
-std::tuple<void *, size_t> socket_parser::read_arr()
-{
-	
+	return {out, len};
 }
