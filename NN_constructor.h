8c4a8babc733e4d67cfd30654783beb4eb113d6f
GGMan <76014247+GGasset@users.noreply.github.com>
2025-12-17 17:51:46 +0100

    Add initialization parameters for NN constructor

--- NN_constructor.h
+++ NN_constructor.h
@@ -11,13 +11,14 @@ private:
 	std::vector<ConnectionTypes> connection_types;
 	std::vector<size_t> layer_lengths;
 	std::vector<ActivationFunctions> activations;
+	std::vector<std::tuple<initialization_parameters, initialization_parameters, initialization_parameters>> initialization;
 public:
 	NN_constructor();
 
 	NN_constructor append_layer(
 		ConnectionTypes connections_type, NeuronTypes neurons_type, size_t neuron_count,
 		ActivationFunctions activation = ActivationFunctions::sigmoid,
-		initialization_parameters connection_weight_init = {.initialization=Xavier},
+		initialization_parameters weight_init = {.initialization=Xavier},
 		initialization_parameters bias_init = {.initialization=constant},
 		initialization_parameters layer_weights_init = {.initialization=Xavier}
 	);
