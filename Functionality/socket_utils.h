bc2ca1cdebe30eacd9b2493517ff8816e8ce8ce4
GGMan <76014247+GGasset@users.noreply.github.com>
2026-01-09 01:16:44 +0100

    Implement parse_apply class for secure parameter parsing and automatic function calling
    Added parse_apply class for parsing parameters with security checks.

--- Functionality/socket_utils.h
+++ Functionality/socket_utils.h
@@ -3,6 +3,7 @@
 #include "stddef.h"
 #include "data_type.h"
 
+#include <type_traits>
 #include <tuple>
 #include <vector>
 
@@ -41,4 +42,81 @@ public:
 	void* expected_read_arr(long expected_len);
 	std::tuple<void *, size_t> read_arr();
 };
+
+// For security reasons and accompanied with pain from the creator
+// After a pointer is passed a size_t which will be used as parsed array len must be specified in template arguments
+// Thus called functions must have an array len parameter after an array pointer
+// An array is preceded (to check read length) and superceded (so it can be checked by the array processing function) (enveloped) in the buffer by its length in size_t
+// The preceding size_t must not be passed as func_param_types, as it will not be passed to the function, its for security purposes only
+template<typename... func_param_types>
+class parse_apply
+{
+private:
+	buff_params params;
+	std::tuple<function_param_types> parameters;
+	std::vector<void*> pointers_to_delete;
+	size_t parsing_param_i = 0;
+	bool must_parse_length = false;
+	size_t last_parsed_len = 0;
+
+	template<typename T>
+	T read_val()
+	{
+		
+	}
+
+	template<typename T>
+	std::tuple<T*,size_t> read_arr()
+	{
+		
+	}
+
+	// throws on error, then recommended to discard the buffer
+	// For security reasons and accompanied with pain from the creator
+// After a pointer is passed a size_t which will be used as parsed array len must be specified in template arguments
+// Thus called functions must have an array len parameter after an array pointer
+// An array is preceded and superceded (enveloped) in the buffer by its length in size_t
+	template<typename param_type>
+	void parse_parameter()
+	{
+		param_type parsed = 0;
+
+		if (std::is_pointer<param_type>::value)
+		{
+			auto [parsed, len] = read_arr<std::remove_pointer<param_type>::type>();
+			last_parsed_len = len;
+			must_parse_length = true;
+		}
+		else
+		{
+			if (!std::is_same_v<param_type, size_t>)
+				assert(!must_parse_length);
+	
+			parsed = read_val<param_type>();
+			if (must_parse_length && last_parsed_len != parsed) throw;
+			
+			parsing_param_i++;
+			must_parse_length = false;
+			last_parsed_len = 0;
+		}
+		return parsed;
+	}
+
+public:
+	parse_apply(void *buff, size_t expected_buff_len);
+	~parse_apply();
+
+	// throws on error, then recommended to discard the buffer
+	template<typename function> // May also return based on a template type (note for future development)
+	auto operator()()
+	{
+		(parse_parameter<func_param_types>(), ...);
+		return std::apply(function, parameters);
+	}
+	
+};
+
+
+
+
 
